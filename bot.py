6c924d77488752e0bc16b240cce382be3212efe3
Dream Journey <mdimrannazirdevloper@gmail.com>
2025-02-20 17:09:38 +0600

    Final For Test

--- bot.py
+++ bot.py
@@ -11,26 +11,6 @@ if not TOKEN:
 if not ETHERSCAN_API_KEY:
     raise ValueError("Etherscan API key is missing or invalid")
 
-
-def main_methode():
-    # Create the Application using the bot's token
-    application = Application.builder().token(TOKEN).build()
-
-    # Response For '/start' command
-    application.add_handler(CommandHandler("start", start_methode))
-
-    # Response For '/gas' command
-    application.add_handler(CommandHandler("gas", get_gas_data))
-
-    # Add a message handler to respond to any message
-    application.add_handler(MessageHandler(None, handle_any_message))
-
-    # Start polling and keep the bot running
-    application.run_polling()
-
-if __name__ == '__main__':
-    main_methode()
-
 async def handle_any_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     is_sticker = bool(update.message.sticker)
     if is_sticker:
@@ -50,7 +30,6 @@ async def start_methode(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         "Hello! I'm your bot. Send any message and I'll reply with the details."
     )
 
-
 async def get_gas_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Fetch the gas price from Etherscan and send it to the user."""
     url = f"https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={ETHERSCAN_API_KEY}"
@@ -66,3 +45,25 @@ async def get_gas_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         await update.message.reply_text(reply_message)
     else:
         await update.message.reply_text("Failed to fetch gas data. Please try again later.")
+
+
+#Main Code HERE
+def main_methode():
+    # Create the Application using the bot's token
+    application = Application.builder().token(TOKEN).build()
+
+    # Response For '/start' command
+    application.add_handler(CommandHandler("start", start_methode))
+
+    # Response For '/gas' command
+    application.add_handler(CommandHandler("gas", get_gas_data))
+
+    # Add a message handler to respond to any message
+    application.add_handler(MessageHandler(None, handle_any_message))
+
+    # Start polling and keep the bot running
+    application.run_polling()
+
+if __name__ == '__main__':
+    main_methode()
+
